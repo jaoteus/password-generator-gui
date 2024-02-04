2001fcb2a49e0121c1bb95a21e565a99daba902d
Mateus <mateusff2323@gmail.com>
2024-02-04 19:51:58 -0300

    Update

--- main.py
+++ main.py
@@ -10,90 +10,193 @@ janelaPrincipal.title("PG")
 # variavéis
 quantidadeDeCaracteres = "0"
 senhaGerada = ""
+selecionadoLetras = IntVar()
+selecionadoNumeros = IntVar()
+selecionadoSimbolos = IntVar()
 
 # Funções:
 def gerarSenha():
-    global senhaGerada
+    global senhaGerada, selecionadoLetras, selecionadoSimbolos, selecionadoNumeros
     senhaGerada = ""
     quantidadeDeCaracteres = localParaColocarQuantidadeDeCaracteres.get()
+
     try:
         quantidadeDeCaracteres = int(quantidadeDeCaracteres)
     except:
-        messagebox.showerror(title="ERROR",
-                             message="VOCÊ NÃO PODE DEIXAR O INPUT VAZIO, COM LETRAS OU COM SÍMBOLOS!"
-                             )
+        messagebox.showerror(title="ERROR", message="VOCÊ NÃO PODE DEIXAR O INPUT VAZIO, COM LETRAS OU COM SÍMBOLOS!")
     else:
         pass
-    while len(senhaGerada) < quantidadeDeCaracteres:
-        letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-                       't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-                       'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-        simbolos = ['!', '@', '#', '$', '%', '&']
-        '''
-        1 - numeros
-        2 - letras
-        3 - simbolos
-        '''
-        qualProximoNumero = random.randrange(1, 4)
-        if qualProximoNumero == 1:
-            algarismoAleatorio = random.choice(letras)
-            senhaGerada = senhaGerada + algarismoAleatorio
-        elif qualProximoNumero == 2:
-            algarismoAleatorio = random.choice(numeros)
-            senhaGerada = senhaGerada + algarismoAleatorio
-        elif qualProximoNumero == 3:
-            algarismoAleatorio = random.choice(simbolos)
-            senhaGerada = senhaGerada + algarismoAleatorio
-        localDaSenhaGerada.delete(1.0, END)
-        localDaSenhaGerada.insert(END, senhaGerada)
 
-# Corpo da janela:
+    # Checando se irá criar com numeros ou letras ou simbolos
+
+    # Todos selecionados:
+    if selecionadoSimbolos.get() == 1 and selecionadoNumeros.get() == 1 and selecionadoLetras.get() == 1:
+        while len(senhaGerada) < quantidadeDeCaracteres:
+            letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                      't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+            simbolos = ['!', '@', '#', '$', '%', '&']
+            '''
+            1 - numeros
+            2 - letras
+            3 - simbolos
+            '''
+            qualProximoNumero = random.randrange(1, 4)
+
+            if qualProximoNumero == 1:
+                algarismoAleatorio = random.choice(letras)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            elif qualProximoNumero == 2:
+                algarismoAleatorio = random.choice(numeros)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            elif qualProximoNumero == 3:
+                algarismoAleatorio = random.choice(simbolos)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            localDaSenhaGerada.delete(1.0, END)
+            localDaSenhaGerada.insert(END, senhaGerada)
+
+    # Apenas numeros e simbolos:
+    elif selecionadoSimbolos.get() == 1 and selecionadoNumeros.get() == 1 and selecionadoLetras.get() == 0:
+        while len(senhaGerada) < quantidadeDeCaracteres:
+            numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+            simbolos = ['!', '@', '#', '$', '%', '&']
+            '''
+            1 - numeros
+            2 - simbolos
+            '''
+            qualProximoNumero = random.randrange(1, 3)
+
+            if qualProximoNumero == 1:
+                algarismoAleatorio = random.choice(numeros)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            elif qualProximoNumero == 2:
+                algarismoAleatorio = random.choice(simbolos)
+                senhaGerada = senhaGerada + algarismoAleatorio
+
+            localDaSenhaGerada.delete(1.0, END)
+            localDaSenhaGerada.insert(END, senhaGerada)
+
+    # Apenas simbolos e letras
+    elif selecionadoSimbolos.get() == 1 and selecionadoNumeros.get() == 0 and selecionadoLetras.get() == 1:
+        while len(senhaGerada) < quantidadeDeCaracteres:
+            letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                      't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            simbolos = ['!', '@', '#', '$', '%', '&']
+            '''
+            1 - letras
+            2 - simbolos
+            '''
+            qualProximoNumero = random.randrange(1, 3)
+
+            if qualProximoNumero == 1:
+                algarismoAleatorio = random.choice(letras)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            elif qualProximoNumero == 2:
+                algarismoAleatorio = random.choice(simbolos)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            localDaSenhaGerada.delete(1.0, END)
+            localDaSenhaGerada.insert(END, senhaGerada)
+
+    # Apenas números e letras
+    elif selecionadoSimbolos.get() == 0 and selecionadoNumeros.get() == 1 and selecionadoLetras.get() == 1:
+        while len(senhaGerada) < quantidadeDeCaracteres:
+            letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                      't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+            '''
+            1 - numeros
+            2 - letras
+            '''
+            qualProximoNumero = random.randrange(1, 3)
+
+            if qualProximoNumero == 1:
+                algarismoAleatorio = random.choice(numeros)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            elif qualProximoNumero == 2:
+                algarismoAleatorio = random.choice(letras)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            localDaSenhaGerada.delete(1.0, END)
+            localDaSenhaGerada.insert(END, senhaGerada)
+
+    # Apenas letras:
+    elif selecionadoSimbolos.get() == 0 and selecionadoNumeros.get() == 0 and selecionadoLetras.get() == 1:
+        while len(senhaGerada) < quantidadeDeCaracteres:
+            letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                      't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            '''
+            1 - letras
+            '''
+            qualProximoNumero = random.randrange(1, 2)
+
+            if qualProximoNumero == 1:
+                algarismoAleatorio = random.choice(letras)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            localDaSenhaGerada.delete(1.0, END)
+            localDaSenhaGerada.insert(END, senhaGerada)
+
+
+    # Apenas números:
+    elif selecionadoSimbolos.get() == 0 and selecionadoNumeros.get() == 1 and selecionadoLetras.get() == 0:
+        while len(senhaGerada) < quantidadeDeCaracteres:
+            numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+            '''
+            1 - numeros
+            '''
+            qualProximoNumero = random.randrange(1, 2)
+
+            if qualProximoNumero == 1:
+                algarismoAleatorio = random.choice(numeros)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            localDaSenhaGerada.delete(1.0, END)
+            localDaSenhaGerada.insert(END, senhaGerada)
+
+    # Apenas símbolos:
+    elif selecionadoSimbolos.get() == 1 and selecionadoNumeros.get() == 0 and selecionadoLetras.get() == 0:
+        while len(senhaGerada) < quantidadeDeCaracteres:
+            letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                      't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            simbolos = ['!', '@', '#', '$', '%', '&']
+            '''
+            1 - simbolos
+            '''
+            qualProximoNumero = random.randrange(1, 2)
+
+            if qualProximoNumero == 1:
+                algarismoAleatorio = random.choice(simbolos)
+                senhaGerada = senhaGerada + algarismoAleatorio
+            localDaSenhaGerada.delete(1.0, END)
+            localDaSenhaGerada.insert(END, senhaGerada)
 
 # LABELS
-labelQuantidadeDeCaracteres = Label(janelaPrincipal,
-                                    text="Quantidade de caracteres:"
-                                    )
-labelQuantidadeDeCaracteres.grid(row=0,
-                                 column=0,
-                                 padx=10,
-                                 pady=10
-                                 )
+labelQuantidadeDeCaracteres = Label(janelaPrincipal, text="Quantidade de caracteres:")
+labelQuantidadeDeCaracteres.grid(row=0, column=0, padx=10, pady=10)
+
 # ENTRADAS
-localParaColocarQuantidadeDeCaracteres = Entry(janelaPrincipal,
-                                               width=5
-                                               )
-localParaColocarQuantidadeDeCaracteres.grid(row=0,
-                                            column=1,
-                                            padx=10,
-                                            pady=10,
-                                            columnspan=1
-                                            )
+localParaColocarQuantidadeDeCaracteres = Entry(janelaPrincipal, width=5)
+localParaColocarQuantidadeDeCaracteres.grid(row=0, column=1, padx=10, pady=10, columnspan=1)
+
 # BOTÕES
-botaoGerarSenha = Button(janelaPrincipal,
-                         text="Gerar senha",
-                         command=gerarSenha
-                         )
-botaoGerarSenha.grid(row=1,
-                     column=0,
-                     padx=10,
-                     pady=10,
-                     columnspan=2
-                     )
+botaoGerarSenha = Button(janelaPrincipal, text="Gerar senha", command=gerarSenha)
+botaoGerarSenha.grid(row=4, column=0, padx=10, pady=10, columnspan=2)
 
 # TEXTOS
-localDaSenhaGerada = Text(janelaPrincipal,
-                          state="normal",  # DEPOIS MUDAR PARA NORMAL
-                          width=30,
-                          height=2
-                          )
-localDaSenhaGerada.grid(row=2,
-                        column=0,
-                        padx=10,
-                        pady=10,
-                        columnspan=2
-                        )
+localDaSenhaGerada = Text(janelaPrincipal,state="normal", width=30,height=2) # DEPOIS MUDAR PARA NORMAL
+localDaSenhaGerada.grid(row=5, column=0, padx=10, pady=10, columnspan=2)
 
+# RADIOS
+radioOpcaoNumeros = Checkbutton(janelaPrincipal, text="Adicionar números", variable=selecionadoNumeros)
+radioOpcaoNumeros.grid(row=1, column=0, padx=0, pady=0)
+
+radioOpcaoLetras = Checkbutton(janelaPrincipal, text="Adicionar letras", variable=selecionadoLetras)
+radioOpcaoLetras.grid(row=2, column=0, padx=0, pady=0)
+
+radioOpcaoSimbolos = Checkbutton(janelaPrincipal, text="Adicionar símbolos", variable=selecionadoSimbolos)
+radioOpcaoSimbolos.grid(row=3, column=0, padx=0, pady=0)
 
 # Loop da janela
 janelaPrincipal.mainloop()
